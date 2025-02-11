92b226211ab55de0db977312a031c8761f730cde
PabloChDAW <pcg0031@alu.medac.es>
2025-02-11 21:38:02 +0100

    Crear proyecto con Vite
    - Creo un nuevo proyecto React con Vite.
    [Documentación moderna de React en español](https://es.react.dev/)
    - Instalo TailwindCSS v3 con Vite.
    [Instalación de TailwindCSS v3 con Vite](https://v3.tailwindcss.com/docs/guides/vite)
    - Instalo la librería Flowbite-React.
    [Instalación de la librería UI Flowbite-React](https://flowbite-react.com/docs/getting-started/introduction)
    - Instalo React Router.
    [Instalación de React Router](https://reactrouter.com/start/library/installation)

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "flowbite-react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <div className="m-40">
+        <Button>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
