dc3471b378cd4e07a4d6a05e078e1ef4d281f3d5
PabloChDAW <pcg0031@alu.medac.es>
2025-02-28 21:48:49 +0100

    Update Detalles.jsx

--- src/pages/Detalles.jsx
+++ src/pages/Detalles.jsx
@@ -13,12 +13,21 @@ const Detalles = () => {
   const { gameDetails } = useLoaderData()
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold">{gameDetails.name}</h1>
-      <img src={gameDetails.background_image} alt={gameDetails.name} />
-      <p>Lanzamiento: {gameDetails.released}</p>
-      <p>Puntuación: {gameDetails.rating}</p>
-      <p>Horas de juego: {gameDetails.playtime}</p>
+    <div className="max-w-4xl mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">{gameDetails.name}</h1>
+      <img src={gameDetails.background_image} alt={gameDetails.name} className="object-cover rounded-lg mb-4" />
+      <div>
+        <p className="text-lg font-semibold">Lanzamiento: {gameDetails.released}</p>
+        <p className="text-lg font-semibold">Puntuación: {gameDetails.rating}</p>
+        <p className="text-lg font-semibold">Horas de juego: {gameDetails.playtime}</p>
+      </div>
+      
+      <h2 className="text-2xl font-bold mt-3 mb-2">Plataformas</h2>
+      <ul className="list-disc list-inside mb-4">
+        {gameDetails.platforms.map((platform) => (
+          <li key={platform.platform.id} className="text-lg">{platform.platform.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
