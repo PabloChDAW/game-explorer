28e1dcad3ebe19f38c714b6ddca3e6aeb98a978e
PabloChDAW <pcg0031@alu.medac.es>
2025-02-28 22:29:36 +0100

    Mejorar Navbar.jsx
    - Ahora los enlaces funcionan usando React-Router.
    - Favicon añadido en index.html

--- src/components/AppNavBar.jsx
+++ src/components/AppNavBar.jsx
@@ -1,25 +1,30 @@
-"use client";
+"use client"
 
-import { Navbar } from "flowbite-react";
+import { Navbar } from "flowbite-react"
+import { Link } from "react-router" // Para que los enlaces funcionen con React Router
 
 const AppNavbar = () => {
   return (
     <Navbar fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
-        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+      <Navbar.Brand href="https://flowbite-react.com"> {/* TODO: Cambiar la URL por la definitiva */}
+        <img
+          width="48"
+          height="48"
+          src="https://img.icons8.com/color/48/sonic-the-hedgehog-1.png"
+          alt="sonic-the-hedgehog"
+          className="mr-4"
+        />
+        <span className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">Game Explorer</span>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#">
-          About
-        </Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
+        <Link to="/" className="navbar-link">
+          <Navbar.Link active={window.location.pathname === '/'}>Principal</Navbar.Link>
+        </Link>
+
+        <Link to="/buscador" className="navbar-link">
+          <Navbar.Link active={window.location.pathname === '/buscador'}>Buscador</Navbar.Link>
+        </Link>
       </Navbar.Collapse>
     </Navbar>
   )
