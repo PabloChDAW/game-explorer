c91705457aa0958815049bcde2ab069f9c1d3bba
PabloChDAW <pcg0031@alu.medac.es>
2025-02-28 23:26:42 +0100

    Update AppFooter.jsx

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -2,12 +2,15 @@ import { Footer } from "flowbite-react";
 
 const AppFooter = () => {
   return (
-    <Footer container className="bg-gray-200">
+    <Footer container className="bg-gray-100">
       <div className="w-full text-center">
         <Footer.Divider />
         <p className="text-gray-600 mb-8 text-sm">
-          Game Explorer. Página web educativa para la investigación sobre aplicaciones reactivas de tipo SPA (Single Page Application). Utiliza las librerías React.JS, React Router y Flowbite. Licencia GNU-GPL3.
+          Game Explorer. Página web educativa para la investigación sobre aplicaciones reactivas de tipo SPA (Single Page Application).
         </p>
+        <p className="text-gray-600 mb-8 text-sm">
+          Utiliza las librerías React.JS, React Router y Flowbite. Licencia GNU-GPL3.
+        </p> 
         <Footer.Copyright by="Game Explorer" year={2025} />
       </div>
     </Footer>
